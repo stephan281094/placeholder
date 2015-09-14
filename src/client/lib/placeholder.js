0df7150c7cf2aff4d44331c74027c6453e799c5b
stephan281094 <stephan281094@gmail.com>
2015-09-14 21:23:26 +0200

    Add options

--- src/client/lib/placeholder.js
+++ src/client/lib/placeholder.js
@@ -1,8 +1,19 @@
 !(function(window, document) {
 
+  var options = {
+    width: 'inherit',
+    height: 'inherit',
+  }
+
   // Constructor
   function Placeholder(el, opts) {
-    this.elements = typeof el === 'object' ? el : document.querySelectorAll(el);
+    if (!opts) {
+      for (var key in opts) {
+          options[key] = opts[key]
+      }
+    }
+
+    this.elements = typeof el === 'object' ? el : document.querySelectorAll(el)
   }
 
   // Public functions
@@ -10,23 +21,23 @@
     _.map(this.elements, function(element) {
       element.style.background = '#' + randomHex({ignore: 'ffffff'})
     })
-  };
+  }
 
   Placeholder.prototype.gradient = function() {
     _.map(this.elements, function(element) {
       var first  = randomHex()
       var second = randomHex()
       var gradient = 'linear-gradient(45deg, #' + first + ', #' + second + ')'
-      element.style.background = gradient;
+      element.style.background = gradient
     })
-  };
+  }
 
   // Helpers
   function randomHex(opts) {
-    var options = opts || {};
+    var options = opts || {}
     var random = Math.floor(Math.random() * 16777215).toString(16)
     return random.length === 6 && !_.contains(options.ignore, random) ? random : randomHex(options)
   }
 
-  window.Placeholder = Placeholder;
-})(window, document);
+  window.Placeholder = Placeholder
+})(window, document)
